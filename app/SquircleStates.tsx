fa1317b0ac0ac7da2e09aab813a6e7c5f5eaf8b7
ragh4uu <raghavshukla498@gmail.com>
2026-06-14 02:40:29 +0530

    feat: establish the states

---
+++ app/SquircleStates.tsx
@@ -0,0 +1,11 @@
+'use client'
+import { useState } from "react";
+
+
+export default function States() {
+    const [n, setN] = useState<number>(4);
+    const [width, setWidth] = useState<number>(100);
+    const [height, setHeight] = useState<number>(100);
+
+  
+}
